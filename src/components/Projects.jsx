7d62eb3ae6df23f248e96556886e7c3823da94e6
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-03-10 17:40:44 +0530

    adding links to projects and changing logo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,39 +6,49 @@ const Projects = () => {
   const projectData=[
     {id:"1",
       name:"Premium Design Taj Royal Restaurant",
-      src:"./assets/project-1.png"
+      src:"./assets/project-1.png",
+      url:"https://thasneem9.github.io/taj-royal-restaurant/"
     },
     {id:"2",
       name:"InstaWave Full Stack Application",
-      src:"./assets/project-2.png"
+      src:"./assets/project-2.png",
+      url:"https://www.youtube.com/watch?v=HR9lszepjRQ"
     },
     {id:"3",
       name:"View Point Tourist Home Website",
-      src:"./assets/project-3.png"
+      src:"./assets/project-3.png",
+      url:"https://thasneem9.github.io/viewPointTouristHome/"
     },
     {id:"4",
       name:"Fun To-Do List Application",
-      src:"./assets/project-4.png"
+      src:"./assets/project-4.png",
+      url:"https://thasneem9.github.io/todoapp/"
     },
     {id:"5",
       name:"Birds and theor Sounds Fun project",
-      src:"./assets/project-5.png"
+      src:"./assets/project-5.png",
+      url:"https://thasneem9.github.io/birdSounds/"
     },
     {id:"6",
       name:"Nike Shoe product Display card",
-      src:"./assets/project-6.png"
+      src:"./assets/project-6.png",
+      url:"https://thasneem9.github.io/3d-card/"
     },
     {id:"7",
       name:"Currency Converter Mini project",
-      src:"./assets/project-7.png"
+      src:"./assets/project-7.png",
+      url:"https://thasneem9.github.io/3d-card/"
+
     },
     {id:"8",
       name:"HTML CSS mini Projects",
-      src:"./assets/project-8.png"
+      src:"./assets/project-8.png",
+      url:"https://thasneem9.github.io/PORTFOLIO"
     },
     {id:"9",
       name:"Full stack Blog application PERN Stack",
-      src:"./assets/project-9.png"
+      src:"./assets/project-9.png",
+      url:"https://youtu.be/rtoxiNASIXA"
     },
   ]
 
@@ -50,7 +60,9 @@ const Projects = () => {
  
     
  {projectData.map((p)=>(
+  <a href={p.url} style={{ textDecoration: "none" }}>
    <ProjectCard key={p.id} name={p.name} src={p.src}/>
+   </a>
  ))
  
  }
