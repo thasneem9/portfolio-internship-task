81c4ab42f26a00f6bd4c980fb633f9e91204c69e
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-04-28 19:27:31 +0530

    added pjects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,38 +1,58 @@
 import React from 'react';
-import { FaEye, FaGithub, FaYoutube, FaFileAlt } from 'react-icons/fa';
+import { FaEye, FaGithub, FaYoutube, FaFileAlt,FaCodeBranch } from 'react-icons/fa';
+
+
 import './projects.css';
 
-const ProjectCard = ({ src, name, preview, code, youtube, readme }) => {
+const ProjectCard = ({ src, name, preview, code, youtube, readme ,prototype}) => {
   return (
     <div className='project-card'>
       <img src={src} alt={name} />
       <h3>{name}</h3>
       <div className="icons-container">
-        {preview && (
-          <div className="icon-item">
-            <FaEye />
-            <span className="icon-text">Preview</span>
-          </div>
-        )}
-        {readme && (
-          <div className="icon-item">
-            <FaFileAlt />
-            <span className="icon-text">Readme</span>
-          </div>
-        )}
-        {code && (
-          <div className="icon-item">
-            <FaGithub />
-            <span className="icon-text">Code</span>
-          </div>
-        )}
-        {youtube && (
-          <div className="icon-item">
-            <FaYoutube />
-            <span className="icon-text">Video</span>
-          </div>
-        )}
-      </div>
+          {preview && (
+            <a href={preview} target="_blank" rel="noopener noreferrer">
+              <div className="icon-item">
+                <FaEye />
+                <span className="icon-text">Preview</span>
+              </div>
+            </a>
+          )}
+          {readme && (
+            <a href={readme} target="_blank" rel="noopener noreferrer">
+              <div className="icon-item">
+                <FaFileAlt />
+                <span className="icon-text">Readme</span>
+              </div>
+            </a>
+          )}
+          {code && (
+            <a href={code} target="_blank" rel="noopener noreferrer">
+              <div className="icon-item">
+                <FaGithub />
+                <span className="icon-text">Code</span>
+              </div>
+            </a>
+          )}
+          {prototype && (
+            <a href={prototype} target="_blank" rel="noopener noreferrer">
+              <div className="icon-item">
+                <FaCodeBranch />
+                <span className="icon-text">Prototype</span>
+              </div>
+            </a>
+          )}
+          {youtube && (
+            <a href={youtube} target="_blank" rel="noopener noreferrer">
+              <div className="icon-item">
+                <FaYoutube />
+                <span className="icon-text">Demo</span>
+              </div>
+            </a>
+          )}
+        </div>
+
+
     </div>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,21 +14,24 @@ import project9 from '../../assets/project-9.png';
 import project10 from '../../assets/project-10.jpg';
 import project11 from '../../assets/project11.jpg';
 import project12 from '../../assets/project12.jpg';
+import project13 from '../../assets/project13.png';
+import project14 from '../../assets/project14.png';
 
 const Projects = () => {
 
   const projectData = [
-    { id: "1", name: "Premium Design Taj Royal Restaurant", src: project1, preview: true, youtube: "https://www.youtube.com/watch?v=HR9lszepjRQ", code: "https://github.com/thasneem9/taj-royal-restaurant", readme: false },
-    { id: "2", name: "InstaWave Full Stack Application", src: project2, preview: true, youtube: "https://www.youtube.com/watch?v=HR9lszepjRQ", code: "https://github.com/thasneem9/instawave", readme: false },
-    { id: "3", name: "View Point Tourist Home Website", src: project3, preview: true, youtube: "", code: "https://github.com/thasneem9/view-point-tourist-home", readme: true },
-    { id: "12", name: "Portfolio Project Interactive UI", src: project12, preview: true, youtube: "", code: "https://github.com/thasneem9/portfolio-nextjs", readme: true },
-    { id: "4", name: "Fun To-Do List Application", src: project4, preview: true, youtube: "", code: "https://github.com/thasneem9/todoapp", readme: true },
-    { id: "5", name: "Birds and Their Sounds Fun Project", src: project5, preview: false, youtube: "", code: "https://github.com/thasneem9/birdSounds", readme: true },
-    { id: "6", name: "Nike Shoe Product Display Card", src: project6, preview: true, youtube: "", code: "https://github.com/thasneem9/3d-card", readme: true },
-    { id: "10", name: "Hair Saloon Website Modern UI", src: project10, preview: true, youtube: "", code: "https://github.com/thasneem9/HairDay-FinalProject", readme: false },
-    { id: "7", name: "Currency Converter Mini Project", src: project7, preview: false, youtube: "", code: "https://github.com/thasneem9/ex231currency", readme: true },
-    { id: "8", name: "HTML CSS Mini Projects", src: project8, preview: true, youtube: "", code: "https://github.com/thasneem9/PORTFOLIO", readme: false },
-    { id: "9", name: "Full Stack Blog Application (PERN Stack)", src: project9, preview: false, youtube: "https://youtu.be/rtoxiNASIXA", code: "https://github.com/thasneem9/PERN-Blog-App", readme: false }
+    { id: "1", name: "Premium Design Taj Royal Restaurant", src: project1, preview: "https://thasneem9.github.io/taj-royal-restaurant/", youtube: "", code: "https://github.com/thasneem9/taj-royal-restaurant", readme: "https://github.com/thasneem9/taj-royal-restaurant/blob/main/README.md" },
+    { id: "2", name: "InstaWave Full Stack Application", src: project2, preview: "https://frontend-production-9386.up.railway.app/", youtube: "https://www.youtube.com/watch?v=HR9lszepjRQ", code: "https://github.com/thasneem9/InstaWave---Copy", readme:"https://github.com/thasneem9/InstaWave---Copy/blob/main/README.md" },
+    { id: "13", name: "Modern GYM website ReactJs", src: project13, preview: "https://thasneem9.github.io/elovon/", youtube: "", code: "https://github.com/thasneem9/elovon", readme: "https://github.com/thasneem9/elovon/blob/master/README.md" },
+    { id: "3", name: "View Point Tourist Home Website", src: project3, preview: "https://thasneem9.github.io/viewPointTouristHome/", youtube: "", code: "https://github.com/thasneem9/view-point-tourist-home", readme: false },
+    { id: "4", name: "Fun To-Do List Application", src: project4, preview: "https://thasneem9.github.io/todoapp/", youtube: "", code: "https://github.com/thasneem9/todoapp", readme: false },
+    { id: "12", name: "Portfolio Project Interactive UI", src: project12, preview: "https://thasneem9.github.io/portfolio-nextjs/", youtube: "", code: "https://github.com/thasneem9/portfolio-nextjs", readme: false },
+    { id: "5", name: "Birds and Their Sounds Fun Project", src: project5, preview: "https://thasneem9.github.io/birdSounds/", youtube: "", code: "https://github.com/thasneem9/birdSounds", readme: false },
+    { id: "10", name: "Hair Saloon Website Modern UI", src: project10, preview: "https://thasneem9.github.io/HairDay-FinalProject/", youtube: "", code: "https://github.com/thasneem9/HairDay-FinalProject", readme: false },
+    { id: "7", name: "Currency Converter Mini Project", src: project7, preview: false, youtube: "", code: "https://github.com/thasneem9/ex231currency", readme: "https://github.com/thasneem9/ex231currency/blob/main/README.md" },
+    { id: "8", name: "HTML CSS Mini Projects", src: project8, preview: "https://thasneem9.github.io/PORTFOLIO", youtube: "", code: "https://github.com/thasneem9/PORTFOLIO", readme: false },
+    { id: "9", name: "Full Stack Blog Application (PERN Stack)", src: project9, preview: "", youtube: "https://youtu.be/rtoxiNASIXA", code: "https://github.com/thasneem9/PERN-Blog-App", readme: false },
+    { id: "14", name: "Leave Mangement System (ongoing)", src: project14, preview: "", youtube: "", code: "" ,readme: "" ,prototype:"https://www.figma.com/design/lmk7EgWxD2xgekn0Q5C54l/Leave-Management-System?node-id=0-1&p=f&t=9aAZMw072ZuJ7XfK-0"}
   ];
 
   return (
@@ -44,6 +47,7 @@ const Projects = () => {
               youtube={p.youtube} 
               code={p.code} 
               readme={p.readme} 
+              prototype={p.prototype}
             />
           </a>
         ))}
